d598b465d64189584d3e191d7378f9aac51a07dd
Dipin Arora <dipinarora401@gmail.com>
2022-06-22 13:34:18 +0530

    modified script to toggle mute

--- bluetooth_profile_changer.sh
+++ bluetooth_profile_changer.sh
@@ -3,12 +3,23 @@
 current_mode_is_a2dp=`pactl list | grep Active | grep a2dp`
 card=`pactl list | grep "Name: bluez_card." | cut -d ' ' -f 2`
 
+if [ -z "$card" ]; then
+    echo "Bluetooth not connected"
+    notify-send "Bluetooth not connected" --expire-time 30
+    exit 0
+fi
+
+pactl set-card-profile $card a2dp-sink
+amixer sset Capture 0%
+amixer set Capture cap
+
 if [ -n "$current_mode_is_a2dp" ]; then
     echo "Switching $card to mSBC (headset, for making calls)..."
-    notify-send "Calls setup" --expire-time 30
     pactl set-card-profile $card headset-head-unit-msbc
+    amixer set Capture cap
+    amixer sset Capture 90%
 else
     echo "Switching $card to A2DP (high-fidelity playback)..."
-    notify-send "Stereo setup" --expire-time 30
     pactl set-card-profile $card a2dp-sink
+    amixer set Capture nocap
 fi
--- current_bluetooth_profile.sh
+++ current_bluetooth_profile.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 current_mode_is_a2dp=`pactl list | grep Active | grep a2dp`
+card=`pactl list | grep "Name: bluez_card." | cut -d ' ' -f 2`
+
+if [ -z "$card" ]; then
+    echo "Bluetooth not connected"
+    notify-send "Bluetooth not connected" --expire-time 30
+    exit 0
+fi
 
 if [ -n "$current_mode_is_a2dp" ]; then
     notify-send "Current setup - Stereo" --expire-time 30
